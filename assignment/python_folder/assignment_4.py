75c7ca81433ca0a814b767ae94d39d8afb847f08
Guilherme Amorim Avilla Gimenez Junior <ggimenezjr@gmail.com>
2019-07-14 16:34:31 -0300

    Fixing small typo regarding lists.

--- assignment/python_folder/assignment_4.py
+++ assignment/python_folder/assignment_4.py
@@ -4,5 +4,5 @@ from utils import mem_address
 if __name__ == "__main__":
     list_var = [1, 55, 3, 350]
 
-    for ix in list_var:
-        mem_address(list_var(ix))
+    for ix in range(len(list_var)):
+        print(f"{list_var[ix]}: {mem_address(list_var[ix])}\n")
